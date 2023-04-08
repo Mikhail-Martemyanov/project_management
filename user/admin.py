617c89e692fbefa6e3954f4fe9658f34031bed0f
mihn23 <martemyanov.mv@mail.ru>
2023-04-08 23:02:27 +0500

    сделал модели + немного оформил админку

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Category)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+
+admin.site.register(User, UserAdmin)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('for_user', 'text_task', 'author_task', 'status')
+
+
+admin.site.register(Task, TaskAdmin)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('author_message', 'text_message', 'create_time')
+
+
+admin.site.register(Message, MessageAdmin)
+
